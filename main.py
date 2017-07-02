af3626b59d96751df171334ffca20e30b296d76e
Alex Nachlas <anachlas@gmail.com>
2017-07-02 16:16:56 -0400

    Used code from https://github.com/GoogleCloudPlatform/python-docs-samples/tree/master/appengine/standard/flask/tutorial to create Flask based python server for App Engine

---
+++ main.py
@@ -0,0 +1,43 @@
+
+# [START app]
+import logging
+
+# [START imports]
+from flask import Flask, render_template, request
+# [END imports]
+
+# [START create_app]
+app = Flask(__name__)
+# [END create_app]
+
+# [START form]
+@app.route('/form')
+def form():
+    return render_template('form.html')
+# [END form]
+
+# [START submitted]
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
+
+    # [END submitted]
+    # [START render_template]
+    return render_template(
+        'submitted_form.html',
+        name=name,
+        email=email,
+        site=site,
+        comments=comments)
+    # [END render_template]
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+# [END app]
