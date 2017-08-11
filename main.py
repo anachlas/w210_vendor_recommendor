8a475688dce2afeb778adb1d671ac97dfb6f6921
Alex Nachlas <anachlas@gmail.com>
2017-08-10 20:08:58 -0400

    Add results page populated with form, remove unused assets

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import logging
 
 # [START imports]
-from flask import Flask, current_app, request
+from flask import Flask, current_app, request, render_template
 # [END imports]
 
 # [START create_app]
@@ -14,6 +14,12 @@ app = Flask(__name__, static_url_path='')
 @app.route('/')
 def main():
     return current_app.send_static_file('index.html')
+
+@app.route('/search', methods=['POST'])
+def hello():
+    req=request.form['req']
+    agency=request.form['agency']
+    return render_template('results.html', name=req, email=agency)
 
 
 @app.errorhandler(500)
